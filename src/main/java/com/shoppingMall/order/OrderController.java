c997e209bc34b193324b308926c409a6726b5b7b
hyeonheee <nloavee@gmail.com>
2024-08-14 01:16:52 +0900

    no message

--- src/main/java/com/shoppingMall/order/OrderController.java
+++ src/main/java/com/shoppingMall/order/OrderController.java
@@ -1,15 +1,22 @@
 package com.shoppingMall.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shoppingMall.user.bo.UserBO;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/shop/order")
 public class OrderController {
+	
 
 	// 주문 메인 
 	@RequestMapping("/order-form-view")
 	public String orderFormView() {
+				
 		return "order/orderForm";
 	}
 }
---
+++ src/main/java/com/shoppingMall/order/OrderRestController.java
@@ -0,0 +1,12 @@
+package com.shoppingMall.order;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shop/order")
+public class OrderRestController {
+
+	// 주문하기 
+	
+}
